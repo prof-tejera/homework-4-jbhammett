1988fd9e1eb895c159859f633e389bfaa5f9f78a
jbhammett <jbeakle@g.clemson.edu>
2023-10-12 00:34:52 -0400

    Implemented and styled calculator.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,143 @@
 import Number from "./Number";
 import Operation from "./Operation";
 import Screen from "./Screen";
+import { useState } from 'react';
 
 const Calculator = () => {
   /** TODO: Here is where you are going to keep track of calculator state */
+  // Track whether we are storing first or second number and rotate.
+  const [rotateNumber,setRotateNumber] = useState(1);
+  const [currentNumber, setCurrentNumber] = useState(null);
+  const [number1, setNumber1] = useState(null);
+  const [number2, setNumber2] = useState(null);
+  const [operator, setOperator] = useState(null);
 
   /** TODO: what happens when I click a number? */
-  const handleNumberClick = () => {};
+  const handleNumberClick = (value) => {  
+    if (rotateNumber == 1) {
+      setNumber1(value);
+      setCurrentNumber(value);
+      setRotateNumber(2);
+      
+    }
+    else {
+      setNumber2(value);
+      setCurrentNumber(value);
+      setRotateNumber(1);
+    }
+  };
 
   /** TODO: what happens when I click an operation? */
-  const handleOperationClick = () => {};
+  const handleOperationClick = (value) => {
+    if (value != "="){
+      setOperator(value);
+    }
+    
+    let answer = null;
+    if (value == "=") {
+      if (operator == "+"){
+        answer = number1 + number2;
+      }
+      else if (operator == "-") {
+        answer = number1 - number2;
+      }
+      else if (operator == "x"){
+        answer = number1 * number2;
+      }
+      else if (operator == "/"){
+        answer = number1 / number2;
+      }
+      setCurrentNumber(answer);
+    }
+    else if (value == "clear"){
+      setNumber1(null);
+      setNumber2(null);
+      setOperator(null);
+      setCurrentNumber(null);
+    }
+  };
 
   return (
-    <div>
-      <Screen value="123" />
-      <div style={{ display: "flex" }}>
-        <div>
-          <Number value={0} onClick={handleNumberClick} />
-          <Number value={1} onClick={handleNumberClick} />
-          <Number value={2} onClick={handleNumberClick} />
-          <Number value={3} onClick={handleNumberClick} />
-          <Number value={4} onClick={handleNumberClick} />
-          <Number value={5} onClick={handleNumberClick} />
-          <Number value={6} onClick={handleNumberClick} />
+    <div style= {{backgroundColor: "#aaaaaa"}}>
+
+      <div 
+        style={{
+
+         marginLeft: "auto",
+         marginRight: "auto",
+         marginBottom: "10px",
+         width: "90%"
+
+        }}>
+        <div
+          style={{ display: "flex",
+                flexDirection: "row", 
+                 justifyContent: "center",
+                }}
+        >    
+          <Screen value={currentNumber} />
+        </div>
+   
+        <div
+          style={{ display: "flex",
+          flexDirection: "row",   
+          justifyContent: "center",   
+          }}
+        >
           <Number value={7} onClick={handleNumberClick} />
           <Number value={8} onClick={handleNumberClick} />
           <Number value={9} onClick={handleNumberClick} />
         </div>
-        <div style={{ paddingLeft: 10 }}>
-          <Operation value="+" onClick={handleOperationClick} />
-          <Operation value="/" onClick={handleOperationClick} />
-          <Operation value="x" onClick={handleOperationClick} />
-          <Operation value="-" onClick={handleOperationClick} />
+        <div
+          style={{ display: "flex",
+          flexDirection: "row",
+          justifyContent: "center"   
+          }}
+        >
+          <Number value={4} onClick={handleNumberClick} />
+          <Number value={5} onClick={handleNumberClick} />
+          <Number value={6} onClick={handleNumberClick} />
+        </div>
+        <div
+          style={{ display: "flex",
+          flexDirection: "row",
+          justifyContent: "center"    
+          }}
+        >
+          <Number value={1} onClick={handleNumberClick} />
+          <Number value={2} onClick={handleNumberClick} />
+          <Number value={3} onClick={handleNumberClick} />
+        </div>
+        <div
+          style={{ display: "flex",
+          flexDirection: "row",   
+          justifyContent: "center" 
+          }}
+        >
           <Operation value="=" onClick={handleOperationClick} />
+          <Number value={0} onClick={handleNumberClick} />
           <Operation value="clear" onClick={handleOperationClick} />
+        </div>
+          
+        <div 
+          style={{ display: "flex",
+          flexDirection: "row",   
+          justifyContent: "center" 
+          }}
+        >
+          <Operation value="+" onClick={handleOperationClick} />
+          <Operation value="-" onClick={handleOperationClick} />
+            
+        </div>
+        <div 
+          style={{ display: "flex",
+          flexDirection: "row",   
+          justifyContent: "center", 
+          }}
+        >
+          <Operation value="x" onClick={handleOperationClick} />
+          <Operation value="/" onClick={handleOperationClick} />
+            
         </div>
       </div>
     </div>
--- src/components/Number.js
+++ src/components/Number.js
@@ -1,15 +1,22 @@
 const Number = ({ value, onClick }) => {
   /** TODO: What happens when a user clicks a number, what do we want to pass to our parent? */
   return (
-    <div
+    <span
+      onClick={()=>onClick(value)}
       style={{
         padding: 10,
-        border: "1px solid black",
+        border: "1px solid #aaaaaa",
         width: 60,
+        backgroundColor: "#00cccc",
+        fontSize: "2rem",
+        color: "#ffffff",
+        flexGrow: 1,
+        textAlign: "center"
+
       }}
     >
       {value}
-    </div>
+    </span>
   );
 };
 
